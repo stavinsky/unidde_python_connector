6586436fdb0ba98967212aaa13b7ce087038a7b0
stavinsky <stavinsky@gmail.com>
2013-08-28 16:18:32 +0300

    simplifying example.py

--- example.py
+++ example.py
@@ -2,19 +2,9 @@ import uniddeconnector
 import params
 
 class MyClient(uniddeconnector.Client):
-    def send_ping(self):
-        pass
     def read_event(self,data):
         print data
-        
-    def custom_handlers(self):
-        self.event=uniddeconnector.Event()
-        
-    def handle_close(self):
-        self.close()
-        self.event.disconnected()
-        self.buffer=''
-        #self.Auth()
+                
 
 
 
@@ -22,3 +12,11 @@ if __name__ == "__main__":
     server = MyClient(params.host, params.port, params.login, params.password)
     server.scheduler.asyncoreLoop(timeout=0.01)
 
+"""
+################### Sample params.py #######################
+#!/usr/bin/python
+login = 'test'
+password = 'test'
+host = 'host'
+port = 2222
+"""
